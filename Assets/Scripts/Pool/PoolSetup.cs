0c4ef25ded721e605d10ffb73275bae91442cd32
snail398 <lololo398@yandex.ru>
2019-07-03 17:02:21 +0300

    first commit

---
+++ Assets/Scripts/Pool/PoolSetup.cs
@@ -0,0 +1,37 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Assets.Scripts.Pool
+{
+    [AddComponentMenu("Pool/PoolSetup")]
+    public class PoolSetup : MonoBehaviour
+    {//обертка для управления статическим классом PoolManager
+
+        #region Unity scene settings
+        [SerializeField] private PoolManager.PoolPart[] pools;
+
+        public ReactiveCommand onPoolReady = new ReactiveCommand();
+        #endregion
+
+        #region Methods
+        void OnValidate()
+        {
+            for (int i = 0; i < pools.Length; i++)
+            {
+                pools[i].name = pools[i].prefab.name;
+            }
+        }
+
+        void Awake()
+        {
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            PoolManager.Initialize(pools);
+            onPoolReady?.Execute();
+        }
+        #endregion
+    }
+}
