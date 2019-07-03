e10121de9b0d20e8c8c72ec0b63157f807a46ae5
snail398 <lololo398@yandex.ru>
2019-07-03 19:48:50 +0300

    start level controller script

---
+++ Assets/Scripts/LevelController/LevelController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    private void Awake()
+    {
+        HeroInit();
+        FeederInit();
+    }
+
+    private void HeroInit()
+    {
+        //Инициализация персонажа
+    }
+
+    private void FeederInit()
+    {
+        //Инициализация податчика ящиков
+    }
+}
